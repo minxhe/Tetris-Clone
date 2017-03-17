bef077296b184c9eef917a36666e85c9dc080f37
Aaron He <aaron.he@windriver.com>
2017-03-17 16:15:23 -0400

    initial commit

---
+++ src/myTetris/Tetris.java
@@ -0,0 +1,39 @@
+package myTetris;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Tetris extends JFrame{
+	
+	
+	private JLabel statusBar;
+	
+	public Tetris(){
+		statusBar = new JLabel("0"); //to display lines number
+		add(statusBar, BorderLayout.SOUTH);
+		Board board = new Board(this);
+		add(board);
+		
+		//start lines down
+		board.start();
+		
+		setSize(200, 400);
+		setTitle("My Tetris");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+	}
+	
+	public JLabel getStatusBar(){
+		return statusBar;
+	}
+	
+	public static void main (String[] args){
+		Tetris myTetris = new Tetris();
+		myTetris.setLocationRelativeTo(null); //center
+		myTetris.setVisible(true);
+		
+		
+	}
+}
